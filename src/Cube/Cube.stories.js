1edc854586d4ef6a82218f9c5b6df84e60643685
Richard Kichenama <rkichenama@gmail.com>
2017-11-25 20:28:04 -0500

    grid is awesome

--- src/Cube/Cube.stories.js
+++ src/Cube/Cube.stories.js
@@ -9,14 +9,7 @@ storiesOf('Cube', module)
   .add('story',
     withInfo('')(
       () => (
-        <Cube>
-          <span>1</span>
-          <span>2</span>
-          <span>3</span>
-          <span>4</span>
-          <span>5</span>
-          <span>6</span>
-        </Cube>
+        <Cube />
       )
     )
 );
