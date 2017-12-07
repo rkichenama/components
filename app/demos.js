839e4ecc1ca45cfe67b3fc5c8c97988f26132bfd
Richard Kichenama <rkichenama@gmail.com>
2017-12-07 16:00:08 -0500

    remove demo list from store, make it dynamic in app. keep watch for reloads

--- app/demos.js
+++ app/demos.js
@@ -4,15 +4,17 @@ import PropTypes from 'prop-types';
 import Catcher from './error';
 import Demo from './demo';
 
+const demoFiles = require.context('./examples', true, /\.jsx?$/i).keys();
+
 export default class Demos extends PureComponent {
   static propTypes = {
-    demos: PropTypes.arrayOf(
-      PropTypes.string
-    ),
+    displayName: PropTypes.string.isRequired,
   };
 
   render () {
-    const { props: { demos }} = this;
+    /* demos must be in top level folder of the components displayName */
+    const regexp = new RegExp(`^./${this.props.displayName}/`, '');
+    const demos = demoFiles.filter(file => regexp.test(file));
 
     return (
       <section>
@@ -21,7 +23,7 @@ export default class Demos extends PureComponent {
           demos.length ? (
             demos.map(file => (
               <Catcher key={file}>
-                <Demo {...{file}} />
+                <Demo file={`app/examples/${file.substring(2)}`} />
               </Catcher>
             ))
           ) : (
