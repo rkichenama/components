b3a8dbc6cdc9519aa8ca87644f77b191f6a5498c
Richard Kichenama <rkichenama@gmail.com>
2018-01-16 10:11:32 -0500

    flatten

--- docs/media/0.1.0/src/DnDExample/Image.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-
-const Image = ({category = 'sports'}) => (
-  <img src={`http://lorempixel.com/400/400/${category}`} />
-);
-
-export default Image;
--- docs/media/0.1.0/storybook/addons.js
+++
@@ -1,5 +0,0 @@
-import '@storybook/addon-options/register';
-// the order they are imported is reflected in the panels 
-import '@storybook/addon-notes/register';
-import '@storybook/addon-knobs/register';
-import '@storybook/addon-actions/register';
