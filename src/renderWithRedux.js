104608d8f9e12aa1ff77d36ad1a84b3f83896cb2
Richard Kichenama <rkichenama@gmail.com>
2018-04-09 14:12:46 -0400

    renderwithredux wrapper for react-testing-library

---
+++ src/renderWithRedux.js
@@ -0,0 +1,20 @@
+// => renderWithRedux.js
+import React from 'react'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import { render } from 'react-testing-library'
+
+/**
+ * usage renderWithRedux(<ConnectedComponent />)
+ */
+const renderWithRedux = ({
+    initialState = {},
+    reducer = state => state,
+    store = createStore(reducer, initialState)
+  } = {}) => componentUnderTest => ({
+  store,
+  ...render(
+    <Provider store={store}>{ componentUnderTest }</Provider>
+  ),
+});
+// => renderWithRedux.js
