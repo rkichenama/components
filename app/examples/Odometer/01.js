b80bf4636370f7011e995f2e18570fab2c5fcebe
Richard Kichenama <rkichenama@gmail.com>
2018-04-12 15:34:42 -0400

    update odometer

---
+++ app/examples/Odometer/01.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Stage } from '../';
+
+import Odometer from 'components/Odometer/Odometer.jsx';
+
+export default class extends React.PureComponent {
+  state = {
+    value: 0
+  }
+
+  componentDidMount () {
+    setInterval(() => this.setState(({ value }) =>
+    ({ value: ++value /* Math.ceil(Math.random() * 9999999) */ })), 2500);
+  }
+
+  render () {
+    return (
+      <Stage style={{ flexBasis: '80%', '--glyph': '96px' }}>
+        <Odometer size={96} value={this.state.value}/>
+      </Stage>
+    );
+  }
+}
--- src/Odometer/Odometer.jsx
+++ src/Odometer/Odometer.jsx
@@ -1,14 +1,120 @@
-import React, { PureComponent } from 'react';
+import React, { Component, PureComponent } from 'react';
 import PropTypes from 'prop-types';
+
+import './Odometer.scss';
+
+class Column extends Component {
+  static propTypes = {
+    place: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
+    size: PropTypes.number.isRequired
+  }
+
+  state = {
+    animating: true
+  }
+
+  componentWillReceiveProps () {
+    this.setState({ animating: true });
+  }
+
+  handleTransitionEnd = ({ target }) => setTimeout(() => this.setState({ animating: false }), 250);
+
+  /**
+   * for a given column index, calucate the distance of travel to the new display digit
+   *
+   */
+  calculateDistance = ({ value, size } = this.props, { animating } = this.state) => -(
+    value + (animating ? 10 : 0)
+  ) * size;
+
+  /**
+   * render the n-th digit
+   * @param {number} n - index of digit to render
+   * @return {Node} - a single digit with multiple digits
+   * */
+  renderDigit = n => (
+    <div key={n} className='digit'>{ n % 10 }</div>
+  );
+
+  /**
+   * render n digits
+   * @param {number} n - number of digits to render
+   * @return {Node} - multiple digit nodes
+   * */
+  renderDigits = n => {
+    const digits = [];
+    for (let i = 0; i < n; ++i)
+      digits.push(this.renderDigit(i));
+    return digits;
+  };
+
+  render () {console.log(this.calculateDistance() / this.props.size);
+    return (
+      <div className={`column${ this.state.animating ? ' animating' : ''}`} style={{ transform: `translateY(${this.calculateDistance()}px)`, transitionDelay: `${50 * this.props.place}ms`}} onTransitionEnd={this.handleTransitionEnd}>
+        {
+          this.renderDigits(20)
+        }
+      </div>
+    )
+  }
+}
 
 /**
  * An odometer that would display a positive numer ina rolling fashion
  */
 export default class Odometer extends PureComponent {
+  static propTypes = {
+    value: PropTypes.number,
+    digits: PropTypes.number,
+    size: PropTypes.number,
+  }
+
+  static defaultProps = {
+    value: 0,
+    digits: 7,
+    size: 96,
+  }
+
+  state = {
+    value: '',
+    animating: [],
+  }
+
+  componentWillReceiveProps ({ value, digits }, init = false) {
+    if (init || (value !== this.props.value)) {
+      this.setState({ value: this.stringify(value, digits), animating: Array(digits).fill(true) })
+    }
+  }
+
+  componentDidMount () { this.componentWillReceiveProps(this.props, true); }
+
+  /**
+   * make the value into a string, front padded with zeros
+   * @param {number} value
+   * @param {number} digits - the width of the string
+   */
+  stringify = (value, digits) => value.toString().padStart(digits, '0');
+
+  /**
+   * render c columns
+   * @param {number} c - number of columns to render
+   * @return {Node} - multiple column nodes
+   * */
+  renderColumns = (c = 7) => {
+    const columns = [];
+    for (let i = 0; i < c; ++i)
+      columns.push((
+        <Column key={i} place={6 - i} value={Number(this.state.value[i] || 0)} size={this.props.size} />
+      ));
+    return columns;
+  };
+
   render () {
     return (
-      <svg>
-      </svg>
+      <div className='odometer' style={{ '--glyph': `${this.props.size}px`}}>
+        { this.renderColumns() }
+      </div>
     );
   }
 }
